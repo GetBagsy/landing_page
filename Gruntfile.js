323440ea017966998a4f38ce1098009a25336953
Zhe Li <zhe.li@jetshop.se>
2015-03-25 17:57:35 +0100

    fixed github site settings

--- Gruntfile.js
+++ Gruntfile.js
@@ -224,7 +224,7 @@ module.exports = function (grunt) {
     buildcontrol: {
       dist: {
         options: {
-          remote: 'git@github.com:zheli/getbagsy.github.io.git',
+          remote: 'git@github.com:GetBagsy/getbagsy.github.io.git',
           branch: 'master',
           commit: true,
           push: true
